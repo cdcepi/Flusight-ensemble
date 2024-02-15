2f5db267f97208c71e2c79176a4c3d667e012de9
smathis <66260252+smathis14@users.noreply.github.com>
2024-02-15 15:50:53 -0500

    adding scripts for quant to pmf

---
+++ create_quant_to_pmf_weekly.R
@@ -0,0 +1,113 @@
+# Create PMF forecasts from baseline and ensemble quantile forecasts 
+# Must run after baseline and ensemble are pushed to the FluSight hub
+
+
+
+#remotes::install_github("Infectious-Disease-Modeling-Hubs/hubUtils")
+library(hubUtils)
+
+# zeallot perform multiple unpacking, and destructing assignment in R.  
+# needed to install disfromq
+# devtools::install_github("https://github.com/nteetor/zeallot")
+
+# distfromq for approximating a distribution's pdf and cdf from a collection
+# of quantiles
+# https://github.com/reichlab/distfromq
+# devtools::install_github("https://github.com/reichlab/distfromq")
+
+library(distfromq)
+
+# reshape2 Flexibily reshape data: a reboot of the reshape package
+# https://github.com/hadley/reshape
+library(reshape2)
+
+# ggforce Accelerating 'ggplot2'
+# https://github.com/thomasp85/ggforce
+library (ggforce)
+
+# tidyverse: Easily install and load the 'Tidyverse'
+# https://github.com/tidyverse/tidyverse
+library(tidyverse)
+
+# readr: Read rectangular text data
+# https://github.com/tidyverse/readr
+#library(readr)
+
+# dplyr: A grammar of data manipulation
+# https://github.com/tidyverse/dplyr
+#library(dplyr)
+
+library(gridExtra)
+
+
+
+# set working directory
+work_dir <- paste0("C:/Users/",Sys.info()["user"],"/Desktop/GitHub/FluSight-ensemble")
+setwd(work_dir)
+
+# source get_pmf_forecasts_from_quantile.R - will eventually be implemented in idforecastutils package
+source("get_pmf_forecasts_from_quantile.R")
+
+# Current category definitions 2023-2024 (as of 2023-11-04 update)
+h_vec <- 0:3 
+multiplier_matrix <- matrix(c(c(2, 3, 4, 5), c(1, 1, 2, 2.5), c(-1, -1, -2, -2.5), c(-2, -3, -4, -5)), ncol=4)
+category_rule_matrix <- matrix(c(rep(10, 4), rep(10, 4), rep(-10, 4), rep(-10, 4)), ncol=4)
+
+# # Initial category definitions 2023-2024 (before 2023-11-04 update)
+# h_vec <- -1:3 
+# multiplier_matrix <- matrix(c(c(2, 3, 4, 5, 5), c(1, 1, 2, 2.5, 2.5), c(-1, -1, -2, -2.5, -2.5), c(-2, -3, -4, -5, -5)), ncol=num_cat-1)
+# category_rule_matrix <- matrix(c(rep(10, 5), rep(10, 5), rep(-10, 5), rep(-10, 5)), ncol=num_cat-1)
+
+# hub function - specify model and get all of the data for that model 
+flusight <- connect_hub(paste0("C:/Users/",Sys.info()["user"],"/Desktop/GitHub/Flusight-forecast-hub"))
+
+cat_data <- flusight %>% filter(output_type == "quantile", horizon != -1) %>%  dplyr::collect() %>%
+  as_model_out_tbl()
+
+flusight_ensemble <- cat_data %>% filter(model_id == "FluSight-ensemble", reference_date == max(reference_date)) 
+flusight_baseline <- cat_data %>% filter(model_id == "FluSight-baseline", reference_date == max(reference_date)) 
+
+location_data <- readr::read_csv(file = "https://raw.githubusercontent.com/cdcepi/FluSight-forecast-hub/main/auxiliary-data/locations.csv") %>% select(-`...5`) %>% rename(geo_value = abbreviation) %>% mutate(geo_value = tolower(geo_value))
+
+target_data <- readr::read_csv(file = "https://raw.githubusercontent.com/cdcepi/FluSight-forecast-hub/main/target-data/target-hospital-admissions.csv") %>% 
+  select(-c(`...1`)) %>% rename(time_value = date)%>%
+  dplyr::inner_join(location_data,
+                    by = join_by("location_name" == "location_name", "location" == "location"))
+
+output_df <- get_pmf_forecasts_from_quantile(
+  quantile_forecasts = flusight_ensemble, locations_df = location_data, truth_df = target_data,
+  
+  #model_output_quantile_only,  
+  #location_data, # locations.csv auxiliary data
+  #target_data, # applicable target data, should have same temporal resolution as model_output
+  categories = c("large_increase", "increase", "stable", "decrease", "large_decrease"),
+  horizons=h_vec, # choose from above
+  count_rate_multiplier=multiplier_matrix, # choose from above 
+  category_rule=category_rule_matrix, # choose from above
+  target_name="wk flu hosp rate change"
+)
+
+output_pmf <- output_df %>% filter(output_type == "pmf") %>% mutate(model_id = "FluSight-ens_q_cat")
+#FluSight-ens_q_cat
+ens_dates <- output_pmf %>% {unique(.$reference_date)}
+
+write.csv(output_pmf, paste0("C://Users/", Sys.info()["user"], "/Desktop/GitHub/FluSight-forecast-hub/model-output/FluSight-q_ens_cat/", ens_dates,"-FluSight-ens_q_cat.csv"), row.names = FALSE)
+
+baseline_output <- get_pmf_forecasts_from_quantile(
+  quantile_forecasts = flusight_baseline, locations_df = location_data, truth_df = target_data,
+  
+  #model_output_quantile_only,  
+  #location_data, # locations.csv auxiliary data
+  #target_data, # applicable target data, should have same temporal resolution as model_output
+  categories = c("large_increase", "increase", "stable", "decrease", "large_decrease"),
+  horizons=h_vec, # choose from above
+  count_rate_multiplier=multiplier_matrix, # choose from above 
+  category_rule=category_rule_matrix, # choose from above
+  target_name="wk flu hosp rate change"
+)
+
+baseline_df <- baseline_output %>% filter(output_type == "pmf") %>% mutate(model_id = "FluSight-baseline_cat")
+
+ens_dates <- baseline_df %>% {unique(.$reference_date)}
+
+write.csv(baseline_df, paste0("C://Users/", Sys.info()["user"], "/Desktop/GitHub/FluSight-forecast-hub/model-output/FluSight-baseline_cat/", ens_dates ,"-FluSight-baseline_cat"), row.names = FALSE)
---
+++ get_pmf_forecasts_from_quantile.R
@@ -0,0 +1,149 @@
+#' Obtain pmf forecasts from quantile forecasts for flu hosp rate change categories
+#'
+#' @param quantile_forecasts a `model_out_tbl` of quantile forecasts with the 
+#'   following columns: `model_id`, `location`, `reference_date`, `horizon`, 
+#'   `target`, `target_end_date`, `output_type`, `output_type_id`, `value`
+#' @param locations_df a data frame of locations to forecast for. Must contain 
+#'   the following columns: `geo_value` (lowercase abbreviations), `location` 
+#'   (fips codes), and `population`
+#' @param truth_df a data frame of truth data with the following columns: 
+#'   `geo_value`, `time_value`, `value`
+#' @param categories a vector of strings containing category names (without spaces).
+#' @param horizons a vector of integers containing the horizons for which categories
+#'   are defined. Defaults to 1, which should be used if categories are not 
+#'   horizon-specific.
+#' @param count_rate_multiplier a matrix containing the count rate multipliers used
+#'   to define categories. Must have dimension length(horizons) x length(categories)
+#' @param category_rule a matrix containing the values against which to compare the
+#'   count rates used to define categories when count rates are below the specified
+#'   values. Must have dimension length(horizons) x length(categories)
+#' @param target_name a string of the desired target name for the output forecasts
+#'
+#' @return a `model_out_tbl` of both pmf and quantile forecasts constructed from the 
+#'   input quantile forecasts with the following columns: `model_id`, `location`,
+#'   `reference_date`, `horizon`, `target`, `target_end_date`, `output_type`, 
+#'   `output_type_id`, `value`
+#' @export
+#'
+#' @examples
+#' 
+#' library(distfromq)
+#' library(ggforce)
+#' library(gridExtra)
+#' library(reshape2)
+#' horizons_2324_new <- 0:3; categories <- c("large_increase", "increase", "stable", "decrease", "large_decrease")
+#' count_rate_multiplier_2324_new <- matrix(c(c(2, 3, 4, 5), c(1, 1, 2, 2.5), c(-1, -1, -2, -2.5), c(-2, -3, -4, -5)), ncol=4)
+#' category_rule_2324_new <- matrix(c(rep(10, 4), rep(10, 4), rep(-10, 4), rep(-10, 4)), ncol=4)
+
+get_pmf_forecasts_from_quantile <- function(quantile_forecasts, locations_df, truth_df, categories, horizons=1, count_rate_multiplier, category_rule, target_name="wk flu hosp rate change") {
+  num_cat = length(categories)
+  
+  truth_df_all <- truth_df %>%
+    dplyr::rename(target_end_date = time_value) %>%
+    dplyr::ungroup() %>%
+    #dplyr::inner_join(locations_df, by = c("geo_value"))  %>% #already joined location df with target data due to geo value being abbreviations
+    dplyr::mutate(model_id="Observed Data", target_variable=target_name, .before=1)
+  
+  # Calculate category boundary values
+  criteria_df_temp <- truth_df_all 
+  criteria_df_all <- NULL
+  for (j in 1:length(horizons)) {
+    criteria_df_temp[["horizon"]] <- horizons[j]
+    for (i in 1:(num_cat-1)) {
+      criteria_df_temp[[paste0("crit", i, sep="")]] <-
+        ifelse(
+          abs(criteria_df_temp[["population"]]*count_rate_multiplier[j,i]/100000) < abs(category_rule[j,i]), 
+          criteria_df_temp[["value"]] + category_rule[j,i], 
+          criteria_df_temp[["value"]] + 
+            round(criteria_df_temp[["population"]]*count_rate_multiplier[j,i]/100000, digits=0)
+        )
+    }
+    criteria_df_all <- bind_rows(criteria_df_all, criteria_df_temp) #changed to bind_rows instead of rbind because we were losing horizon
+  }
+  
+  criteria_df_all <- criteria_df_all %>%
+    dplyr::select(model_id, location, value, target_end_date, horizon,target_variable, population, crit1:ncol(criteria_df_all)) %>%
+    dplyr::filter(!is.na(value))
+  
+  train_forecasts <- criteria_df_all %>%
+    dplyr::select(location, horizon, target_end_date, target_variable, population, crit1:ncol(criteria_df_all)) %>%
+    dplyr::mutate(date=target_end_date+weeks(1), target_end_date = date+weeks(horizon), .before = 3) 
+  
+  
+  # extract log pdf and cdf values for training set forecasts
+  # we add a little noise to the value column so that there is a density to
+  # work with in case the forecaster had a point mass anywhere
+  quantile_forecasts <- quantile_forecasts %>%
+    dplyr::mutate(reference_date=as.Date(reference_date)) 
+  
+  # filter for dates, horizons, locations to forecast for
+  criteria_df_filtered <- train_forecasts %>%
+    dplyr::inner_join(
+      quantile_forecasts,
+      by = c("date"="reference_date", "horizon", "target_end_date", "location")
+    ) %>% mutate(output_type_id = as.numeric(output_type_id)) #getting errors this wasn't numeric
+  
+  # filter for dates, horizons, locations to forecast for (no distinct output_type_ids)
+  train_forecasts <- criteria_df_filtered %>%
+    dplyr::distinct(model_id, location, date, horizon, target, .keep_all=TRUE) %>%
+    dplyr::select(-target, -output_type,-output_type_id,-value)
+  
+  # Calculate cdf category boundary values
+  for (i in 1:(num_cat-1)) {
+    criteria_df_filtered[["crit_current"]] <- criteria_df_filtered[[paste0("crit", i, sep="")]] 
+    train_temp <- criteria_df_filtered %>%
+      dplyr::group_by(model_id, date, location, horizon, target, target_end_date) %>%
+      dplyr::summarize(
+        cdf_crit_current = distfromq::make_p_fn(
+          ps = output_type_id,
+          qs = value)(unique(crit_current), log = FALSE)
+      ) %>%
+      dplyr::ungroup() %>%
+      dplyr::select(-c(target, target_end_date))
+    train_forecasts[[paste0("cdf_crit", i)]] <- train_temp[["cdf_crit_current"]]
+  }
+  
+  #calculate category percentages from cdf criteria, correcting for negative numbers
+  exp_forecast <- train_forecasts %>%
+    dplyr::ungroup() %>%
+    dplyr::rename(reference_date=date, target=target_variable) %>%
+    dplyr::mutate(cdf_crit0=1, .before=cdf_crit1)
+  
+  exp_forecast[[paste0("cdf_crit", num_cat)]] <- exp_forecast[[paste0("crit", num_cat)]] <- 0
+  cdf_crit_sum <- 0
+  for (i in 1:(num_cat)) {
+    if (cdf_crit_sum < 1) {
+      exp_forecast[[categories[i]]] <- exp_forecast[[paste0("cdf_crit", i-1)]] - ifelse(exp_forecast[[paste0("crit", i)]] > 0, exp_forecast[[paste0("cdf_crit", i)]], 0)
+    } else {
+      exp_forecast[[categories[i]]] <- 0
+    }
+    cdf_crit_sum <- cdf_crit_sum + mean(exp_forecast[[categories[i]]])
+  }
+  
+  exp_forecast <- exp_forecast %>%
+    dplyr::select(model_id,reference_date,location,horizon, all_of(categories))
+  
+  
+  #transpose data_frame to format for submission
+  exp_t = melt(
+    exp_forecast,
+    id.vars = c("model_id","reference_date","location","horizon"),
+    measure.vars = categories,
+    variable.name="output_type_id",
+    value.name="value"
+  )
+  exp_t <- exp_t %>%
+    dplyr::mutate(target=target_name, output_type="pmf") %>%
+    dplyr::select(model_id, reference_date, horizon, target, location, output_type, output_type_id, value)
+  
+  output_forecasts <- exp_t %>%
+    dplyr::mutate(
+      output_type_id=as.character(output_type_id),
+      target_end_date=reference_date+weeks(horizon)
+    ) %>%
+    dplyr::bind_rows(mutate(quantile_forecasts, output_type_id=as.character(output_type_id)))%>%
+    dplyr::filter(location %in% pull(locations_df, location)) %>%
+    dplyr::select(model_id, reference_date, horizon, target, target_end_date, location, output_type, output_type_id, value)
+  
+  return (output_forecasts)
+}
