3b53a66a18958d9d5664e740d8c3b712f0422fab
Annabella Hines <annabellaghines@gmail.com>
2024-11-05 15:05:10 -0500

    2024/25 Season Updates
    -Archived 2023/24 model outputs and model inclusion files into Archive_2324 folder.
    -Added code to generate ensembles for peak week and peak intensity targets and integrate them into final ensemble file.
    -Added code for quantile targets to round value down to nearest whole number if in a quantile under 0.5, and round up if in a quantile 0.5 or higher.
    -Added linear pool model for peak intensity quantile target

--- generate_flu_ensemble.R
+++ generate_flu_ensemble.R
@@ -92,9 +92,10 @@ models = as.character(eligible_models$Model)
 current_forecasts <- current_forecasts[current_forecasts$model_id %in% models,]
 current_forecasts <- current_forecasts[current_forecasts$location != 78,]
 
-# QUANTILE ENSEMBLE
+# QUANTILE 1-4 WEEK AHEAD ENSEMBLE
 quantile_forecasts <- current_forecasts |>
   dplyr::filter(output_type == "quantile") |>
+  dplyr::filter(target == "wk inc flu hosp") |>
   dplyr::mutate(output_type_id=as.character(as.numeric(output_type_id))) # ensures quantiles treated the same regardless of presence of trailing zeros
   
 # generate median ensemble
@@ -105,11 +106,45 @@ median_ensemble_outputs <- quantile_forecasts |>
     model_id=median_name, 
     task_id_cols=task_id_cols
   ) |>
+  dplyr::mutate(
+    # Apply rounding conditions based on quantile value
+    value = ifelse(
+      output_type_id < 0.5,
+      floor(value),    # Round down if quantile is below 0.5
+      ceiling(value)   # Round up if quantile is 0.5 or above
+    ),
+  ) |>
   dplyr::mutate(value = ifelse(value < 0, 0, value)) |>
   dplyr::select(-model_id)
 
 # median_ensemble_path <- paste(hub_path, "/model-output/", median_name, "/", current_ref_date, "-", median_name, ".csv", sep="") 
 # readr::write_csv(median_ensemble_outputs, median_ensemble_path)
+
+# QUANTILE PEAK INTENSITY ENSEMBLE
+quantile_intensity_forecasts <- current_forecasts |>
+  dplyr::filter(output_type == "quantile") |>
+  dplyr::filter(target == "peak inc flu hosp") |>
+  dplyr::mutate(output_type_id=as.character(as.numeric(output_type_id))) # ensures quantiles treated the same regardless of presence of trailing zeros
+
+# generate median ensemble
+median_intensity_name <- "FluSight-Intensity-median"
+median_intensity_ensemble_outputs <- quantile_intensity_forecasts |>
+  hubEnsembles::simple_ensemble(
+    agg_fun="median", 
+    model_id=median_intensity_name, 
+    task_id_cols=task_id_cols
+  ) |>
+  dplyr::mutate(
+    # Apply rounding conditions based on quantile value
+    value = ifelse(
+      output_type_id < 0.5,
+      floor(value),    # Round down if quantile is below 0.5
+      ceiling(value)   # Round up if quantile is 0.5 or above
+    ),
+  ) |>
+  dplyr::mutate(value = ifelse(value < 0, 0, value)) |>
+  dplyr::select(-model_id)
+
   
 # generate linear pool of quantiles (if desired)
 lop_norm_name <- "FluSight-lop_norm"
@@ -121,18 +156,44 @@ lop_norm_outputs <- quantile_forecasts |>
   ) |>
   dplyr::mutate(
     value = ifelse(value < 0, 0, value), 
-    output_type_id = as.character(output_type_id)
-  ) |>
+    output_type_id = as.character(output_type_id),
+    value = ifelse(
+      output_type_id < 0.5,
+      floor(value),    # Round down if quantile is below 0.5
+      ceiling(value)   # Round up if quantile is 0.5 or above
+  )) |>
   dplyr::select(-model_id)
 
+# geneate linear pool of intensity target quantiles
+lop_norm_intensity_name <- "FluSight-lop_norm-Intensity"
+lop_norm_intensity_outputs <- quantile_intensity_forecasts |>
+  dplyr::mutate(output_type_id=as.numeric(output_type_id)) |>
+  hubEnsembles::linear_pool(
+    model_id=lop_norm_intensity_name, 
+    task_id_cols=task_id_cols
+  ) |>
+  dplyr::mutate(
+    value = ifelse(value < 0, 0, value), 
+    output_type_id = as.character(output_type_id),
+    value = ifelse(
+      output_type_id < 0.5,
+      floor(value),    # Round down if quantile is below 0.5
+      ceiling(value)   # Round up if quantile is 0.5 or above
+    )) |>
+  dplyr::select(-model_id)
+
+# combine linear pool outputs for both quantile targets
+lop_norm_outputs_combined<-dplyr::bind_rows(lop_norm_outputs, lop_norm_intensity_outputs)
+
 lop_norm_path <- paste(out_path, "/model-output/", lop_norm_name, "/", current_ref_date, "-", lop_norm_name, ".csv", sep="")
-readr::write_csv(lop_norm_outputs, lop_norm_path)
+readr::write_csv(lop_norm_outputs_combined, lop_norm_path)
 
 
-# PMF ENSEMBLE
+# PMF CATEGORICAL ENSEMBLE
 categorical_name <- "FluSight-categorical"
 categorical_forecasts <- current_forecasts |>
   dplyr::filter(output_type == "pmf") |>
+  dplyr::filter(target == "wk flu hosp rate change") |>
   dplyr::group_by(reference_date, target, target_end_date, output_type) |> # create appropriate groups for `complete`
   tidyr::complete(model_id, horizon, location, output_type_id, fill=list(value=NA)) |> # add in missing output_type_ids and fill the missing values with zero
   unique()
@@ -147,9 +208,29 @@ categorical_ensemble_outputs <- categorical_forecasts |>
   ) |>
   dplyr::select(-model_id)
 
+# PMF PEAK WEEK ENSEMBLE
+peak_week_name <- "FluSight-Peak-Week"
+peak_week_forecasts <- current_forecasts |>
+  dplyr::filter(output_type == "pmf") |>
+  dplyr::filter(target == "peak week inc flu hosp") |>
+  dplyr::group_by(reference_date, target, output_type) |> # create appropriate groups for `complete`
+  tidyr::complete(model_id, location, output_type_id, fill=list(value=NA)) |> # add in missing output_type_ids and fill the missing values with zero
+  unique()
+
+peak_week_ensemble_outputs <- peak_week_forecasts |>
+  hubEnsembles::simple_ensemble(
+    agg_fun="mean", 
+    agg_args = list(na.rm = T),
+    model_id=peak_week_name, 
+    task_id_cols=task_id_cols
+  ) |>
+  dplyr::select(-model_id)
+
 ensemble_name <- "FluSight-ensemble"
 flusight_ensemble_outputs <- median_ensemble_outputs |>
-  dplyr::bind_rows(categorical_ensemble_outputs)
+  dplyr::bind_rows(categorical_ensemble_outputs) |>
+  dplyr::bind_rows(median_intensity_ensemble_outputs) |>
+  dplyr::bind_rows(peak_week_ensemble_outputs)
 flusight_ensemble_path <- paste(out_path, "/model-output/", ensemble_name, "/", current_ref_date, "-", ensemble_name, ".csv", sep="") 
 readr::write_csv(flusight_ensemble_outputs, flusight_ensemble_path)
 
